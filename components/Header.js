40642207042c578bd08a95b7379f9b70460d1396
Princewill Iroka <princewilliroka@outlook.com>
2020-12-19 02:53:31 +0100

    Added Header component

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,10 @@
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <div className="flex-row py-2">
+      <img
+        src="/assets/domain-trader-logo.png"
+        className="w-20 h-20 object-contain"
+      />
+    </div>
+  );
 }
